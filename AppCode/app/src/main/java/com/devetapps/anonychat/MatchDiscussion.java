2e68d4987afb19380cd64ad804c8f33da3ca564d
Theo Grenet (i2200213) <i2200213@univ-lemans.fr>
2024-03-14 12:11:38 +0100

    Légères modifs côté backend + Développement connexion, changement de pseudo, et peut-être envoi de messages côté client

---
+++ AppCode/app/src/main/java/com/devetapps/anonychat/MatchDiscussion.java
@@ -0,0 +1,14 @@
+package com.devetapps.anonychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchDiscussion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_discussion);
+    }
+}
